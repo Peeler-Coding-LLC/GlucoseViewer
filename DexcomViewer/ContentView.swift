1d8e20416202ffa37e92d7e5d2763c6481d5d342
Chase Peeler <chasepeeler@gmail.com>
2023-01-10 21:56:39 -0500

    first commit

---
+++ DexcomViewer/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DexcomViewer
+//
+//  Created by Chase Peeler on 1/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DexcomViewer/DexcomViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DexcomViewerApp.swift
+//  DexcomViewer
+//
+//  Created by Chase Peeler on 1/10/23.
+//
+
+import SwiftUI
+
+@main
+struct DexcomViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
